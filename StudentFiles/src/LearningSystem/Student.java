0c20126ae92f3845cfa5f8601dd66a2699ccc1a2
Shinfu01 <163119165+Shinfu01@users.noreply.github.com>
2025-04-12 13:42:32 +0800

    SF#2

--- StudentFiles/src/LearningSystem/Student.java
+++ StudentFiles/src/LearningSystem/Student.java
@@ -36,7 +36,7 @@ public class Student extends JFrame {
 	 */
 	public Student() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 696, 528);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -45,32 +45,37 @@ public class Student extends JFrame {
 		
 		JButton btnNewButton = new JButton("DSA77");
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnNewButton.setBounds(31, 98, 154, 55);
+		btnNewButton.setBounds(37, 186, 221, 63);
 		contentPane.add(btnNewButton);
 		
 		JButton btnCp = new JButton("CP332");
 		btnCp.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnCp.setBounds(31, 22, 154, 55);
+		btnCp.setBounds(37, 113, 221, 63);
 		contentPane.add(btnCp);
 		
 		JButton btnDm = new JButton("DM172");
 		btnDm.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnDm.setBounds(31, 173, 154, 55);
+		btnDm.setBounds(37, 259, 221, 63);
 		contentPane.add(btnDm);
 		
 		JLabel lblNewLabel = new JLabel("Ocampo, Cobain");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(288, 183, 196, 34);
+		lblNewLabel.setBounds(456, 229, 135, 34);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("415862");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_1.setBounds(313, 208, 58, 34);
+		lblNewLabel_1.setBounds(494, 259, 58, 34);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon("D:\\SHAWN\\Eclipse Projects\\StudentFiles\\images\\ProfileImage1.png"));
-		lblNewLabel_2.setBounds(276, 46, 135, 136);
+		lblNewLabel_2.setBounds(446, 83, 135, 136);
 		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Student Info");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		lblNewLabel_3.setBounds(247, 10, 183, 63);
+		contentPane.add(lblNewLabel_3);
 	}
 }
